4bb960a44a49b3df6d4336ab02f6f5f92a4bbc5f
AADecoz <82182504+AADecoz@users.noreply.github.com>
2021-11-08 19:35:59 +0100

    adjustment typewriter z-index

--- src/app/menu-portfolio/menu-portfolio.component.ts
+++ src/app/menu-portfolio/menu-portfolio.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import {faChevronDown, faChevronRight} from "@fortawesome/free-solid-svg-icons";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-menu-portfolio',
   templateUrl: './menu-portfolio.component.html',
-  styleUrls: ['./menu-portfolio.component.scss']
+  styleUrls: ['./menu-portfolio.component.scss'],
+  animations:[
+    trigger("openClose", [
+     transition(":enter", [
+        style({
+          transform: "rotate(0)"
+        }),
+        animate("200ms ease-out", style({
+          transform: "rotate(0)"
+        }))
+      ]),
+      transition(":leave", [
+        style({
+          transform: "rotate(-90deg)"
+        }),
+        animate("200ms ease-out", style({
+          transform: "rotate(-90deg)"
+        }))
+      ])
+    ])
+
+  ]
 })
 export class MenuPortfolioComponent implements OnInit {
+  fachevronright = faChevronRight;
+  fachevrondown = faChevronDown;
+  dropDown = false;
+
+
+
 
   constructor() { }
 
+
   ngOnInit(): void {
+    this.dropDown = !this.dropDown
   }
 
 }
